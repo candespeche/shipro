8b2e439ce09cbf668b8f195aaaedc6f000cc80e2
candespeche <53618928+candespeche@users.noreply.github.com>
2020-09-07 10:23:46 -0300

    hero slide margin

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../Common/Navbar/Navbar"
-import Hero from "../Hero/Hero"
+import Hero from "./Hero/Hero"
 import QueEs from "./QueEs/QueEs"
 import Clientes from "./Clientes/Clientes"
 import Footer from "../Common/Footer/Footer"
